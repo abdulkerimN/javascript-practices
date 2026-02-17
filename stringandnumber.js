1c90cc85a75bec6791e197d08281e54c3407680d
abdu <abdualkerim01@gmail.com>
2026-02-17 09:52:51 +0300

    strings and numbers

---
+++ stringandnumber.js
@@ -0,0 +1,6 @@
+console.log("2" + 2); //22
+console.log(11 + 7); //18
+console.log(6 + "5"); //65
+console.log("My points: " + 5 + 9); // not 14, but My points: 59 because of the string concatenation
+console.log(2 + 2); //4
+console.log("11" + "14"); //1114
