a87be37b6621f9d604cdea108d58143dbfc6607a
abdu <abdualkerim01@gmail.com>
2026-02-17 10:27:28 +0300

    rendering

---
+++ rendering errorM/index.js
@@ -0,0 +1,9 @@
+const shoeError = document.getElementById("error");
+
+function seeError() {
+    result = shoeError.textContent = "Something went wrong, please try again";
+    console.log("Something went wrong, please try again");
+    // alert("Something went wrong, please try again");
+
+    // alert(result);
+}
