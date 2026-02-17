4070d73445620e8238485cf75ed2d27e903c7911
abdu <abdualkerim01@gmail.com>
2026-02-17 10:36:09 +0300

    rendering the shoe text to the new error text

--- rendering errorM/index.js
+++ rendering errorM/index.js
@@ -1,9 +1,5 @@
 const shoeError = document.getElementById("error");
 
 function seeError() {
-    result = shoeError.textContent = "Something went wrong, please try again";
-    console.log("Something went wrong, please try again");
-    // alert("Something went wrong, please try again");
-
-    // alert(result);
+    shoeError.textContent = "Something went wrong, please try again";
 }
