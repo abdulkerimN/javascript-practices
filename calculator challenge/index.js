ae52c11c5be46a036c762afadca7806de161129c
abdu <abdualkerim01@gmail.com>
2026-02-17 11:15:45 +0300

    the 4 functions performs their calculation

--- calculator challenge/index.js
+++ calculator challenge/index.js
@@ -1,13 +1,27 @@
 let kutr1 = 10
 let kutr2 = 5
+let dmr = document.getElementById("sum-el");
 
 document.getElementById("kutr1-el").textContent = kutr1
 document.getElementById("kutr2-el").textContent = kutr2
 
-function add() {}
 
-function subtract() {}
+function add() {
+    result = kutr1 + kutr2
+    dmr.textContent = "Sum: " + result
+}
 
-function divide() {}
+function subtract() {
+    result = kutr1 - kutr2;
+    dmr.textContent = "Sum: " + result;
+}
 
-function multiply() {}
+function divide() {
+    result = kutr1 / kutr2;
+    dmr.textContent = "Sum: " + result;
+}
+
+function multiply() {
+    result = kutr1 * kutr2;
+    dmr.textContent = "Sum: " + result;
+}
