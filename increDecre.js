d53a5a8ea537ebf88f1f55eabec1a14253562305
abdu <abdualkerim01@gmail.com>
2026-02-17 09:49:00 +0300

    incre and decre ment

---
+++ increDecre.js
@@ -0,0 +1,36 @@
+// let myPoints = 3;
+
+// function add3Points() {
+//     myPoints += 3;
+//     console.log(myPoints);
+// }
+
+// add3Points();
+// add3Points();
+// add3Points();
+
+// function remove1Point() {
+//     myPoints -= 1;
+//     console.log(myPoints);
+// }
+// remove1Point();
+// remove1Point();
+
+
+let myPoints = 3;
+
+function add3Points() {
+    myPoints += 3;
+}
+
+add3Points();
+add3Points();
+add3Points();
+
+function remove1Point() {
+    myPoints -= 1;
+}
+remove1Point();
+remove1Point();
+
+console.log(myPoints);
