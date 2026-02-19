395348dbaeb9783a51d3bdddbde88abeb0c2aa98
abdu <abdualkerim01@gmail.com>
2026-02-19 15:39:18 +0300

    player added

--- blackjack-game/index.js
+++ blackjack-game/index.js
@@ -6,6 +6,10 @@ sum = 0
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el");
+let playerName = "bogale"
+let playerMoney = 200
+let playerEl = document.getElementById("techawach");
+playerEl.textContent = playerName + ": $" + playerMoney
 
 function getRandomCard() {
     let randomNumber = Math.floor(Math.random() * 13) + 1
