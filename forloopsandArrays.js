32c935a8fe371ad2bbbe0c0f8f93e98d5c99ed37
abdu <abdualkerim01@gmail.com>
2026-02-19 11:02:20 +0300

    length added

--- forloopsandArrays.js
+++ forloopsandArrays.js
@@ -1,5 +1,9 @@
-let message = ["Hey, how are you?", "I'm fine, thanks!", "What about you?"]
+let message = ["Hey, how are you?", "I'm fine, thanks!", "What about you?", "i am just learning JavaScript!", "and its good!"]
 
-for (let i = 0; i < 3; i += 1) {
+// for (let i = 0; i < 3; i += 1) {
+//     console.log(message[i])
+// }
+
+for (let i = 0; i < message.length; i += 1) {
     console.log(message[i])
 }
