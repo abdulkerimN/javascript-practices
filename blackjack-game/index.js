0dca10256a24862e80ec54c494f9060a7f453270
abdu <abdualkerim01@gmail.com>
2026-02-19 09:03:40 +0300

    update the value of 1st and 2nd card to align with the functions logic

--- blackjack-game/index.js
+++ blackjack-game/index.js
@@ -1,4 +1,4 @@
-let firstCard = 12
+let firstCard = 1
 let secondCard = 10
 let hasBlackJack = false
 let isAlive = true
@@ -10,9 +10,9 @@ let cardsEl = document.getElementById("cards-el");
 sum = firstCard + secondCard
 
 function startGame() {
-    console.log("Starting the game...");
-    sumEl.textContent = "Sum: " + sum;
+    // console.log("Starting the game...");
     cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+    sumEl.textContent = "Sum: " + sum;
     if (sum < 21) {
         message = "Do you want to draw a new card? 🙂";
     } else
@@ -26,15 +26,18 @@ function startGame() {
     messageEl.textContent = message;
 }
 
-console.log(sum);
-startGame();
-console.log(message);
-console.log(hasBlackJack);
-console.log(isAlive);
+// console.log(sum);
+// startGame();
+// console.log(message);
+// console.log(hasBlackJack);
+// console.log(isAlive);
 
 
 function newCard() {
-    console.log("Drawing a new card from the deck!")
+    // console.log("Drawing a new card from the deck!")
+    let card = 2
+    sum += card
+    startGame()
 }
 // let haveEaten = true
 
